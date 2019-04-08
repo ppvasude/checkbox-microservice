bb19c81538aeed8e26cff5d6dc4788bbcf3476e6
Purva Vasudeo <ppvasude@ncsu.edu>
2019-04-08 18:47:11 -0400

    Initial commit

---
+++ server-side/site/server.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+        cors = require('cors'),
+	marqdown = require('./marqdown.js'),
+	//routes = require('./routes/designer.js'),
+	//votes = require('./routes/live.js'),
+	//upload = require('./routes/upload.js'),
+	// create = require('./routes/create.js'),
+	// study = require('./routes/study.js'),
+	// admin = require('./routes/admin.js');
+
+var app = express();
+
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
+var whitelist = ['http://chrisparnin.me', 'http://pythontutor.com', 'http://happyface.io', 'http://happyface.io:8003', 'http://happyface.io/hf.html'];
+var corsOptions = {
+  origin: function(origin, callback){
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  }
+};
+
+app.options('/api/study/vote/submit/', cors(corsOptions));
+
+app.post('/api/design/survey', 
+	function(req,res)
+	{
+		console.log(req.body.markdown);
+		//var text = marqdown.render( req.query.markdown );
+		var text = marqdown.render( req.body.markdown );
+		res.send( {preview: text} );
+	}
+);
+
+var port = process.env.APP_PORT;
+app.listen(port);
+console.log(`Listening on port ${port}...`);
