a95c127257174a865bba9414d426416187fa2350
ppvasude <44244649+ppvasude@users.noreply.github.com>
2019-04-10 20:35:34 -0400

    Update server.js

--- server-side/site/server.js
+++ server-side/site/server.js
@@ -31,7 +31,7 @@ app.post('/api/design/survey',
 		console.log(req.body.markdown);
 		//var text = marqdown.render( req.query.markdown );
 		var text = marqdown.render( req.body.markdown );
-		res.send('<h1>Hi from Service</h1>');
+		res.send( {preview: text} );
 	}
 );
 
